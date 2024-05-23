7327a49259fd0a144558019a6dab870df2a4eaf7
faithannenjambi <114464479+faithannenjambi@users.noreply.github.com>
2024-05-23 18:58:43 +0300

    created the log in/sign up page

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ document.addEventListener('DOMContentLoaded', () =>
             menu_navigations.classList.toggle('hide-mobile-menu-navigations');
         })
 })
+
+window.onscroll = () => {
+    change_nav_background();
+    hide_menu();
+};
+
+function change_nav_background()
+{
+    var navbar = document.querySelector("#navbar");
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 )
+        {
+            navbar.classList.add("nav-background");
+        }
+    else
+    {
+        navbar.classList.remove("nav-background");
+    }    
+}
+
+function hide_menu()
+{
+    menu_navigations.classList.add("hide-mobile-menu-navigations");
+}
